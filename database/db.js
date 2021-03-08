f02a8e0f724fbe9748680a86398241aca98f6769
Nelson Ly <nelsonly123@gmail.com>
2021-03-07 21:32:20 -0800

    made seeding functions async, added db refresh functionality

--- database/db.js
+++ database/db.js
@@ -24,28 +24,47 @@ let reviewSchema = new mongoose.Schema({
 
 let Reviews = mongoose.model('Reviews', reviewSchema);
 
-let seedDB = () => {
-  for (let i = 0; i < 50; i++) {
-    let newReview = new Reviews({
-      userName: faker.name.findName(),
-      dateJoined: faker.date.month() + ' 2021',
-      profilePic: 'Insert SW url here',
-      reviewDescription: faker.lorem.sentences(3),
-      reviewRating: [{
-        cleanliness: faker.finance.amount(2,5,1),
-        communication: faker.finance.amount(2,5,1),
-        checkIn: faker.finance.amount(2,5,1),
-        accuracy: faker.finance.amount(2,5,1),
-        location: faker.finance.amount(2,5,1),
-        value: faker.finance.amount(2,5,1),
-      }],
-      listingID: faker.random.number(100)
-    });
-    Reviews.create(newReview);
+let seedReviewsDB = () => {
+  return new Promise((resolve, reject) => {
+    for (let i = 0; i < 100; i++){
+      let newReview = new Reviews({
+        userName: faker.name.findName(),
+        dateJoined: faker.date.month() + ' 2021',
+        profilePic: 'Insert SW url here',
+        reviewDescription: faker.lorem.sentences(3),
+        reviewRating: [{
+          cleanliness: faker.finance.amount(2,5,1),
+          communication: faker.finance.amount(2,5,1),
+          checkIn: faker.finance.amount(2,5,1),
+          accuracy: faker.finance.amount(2,5,1),
+          location: faker.finance.amount(2,5,1),
+          value: faker.finance.amount(2,5,1),
+        }],
+        listingID: faker.random.number(100)
+      });
+      resolve(Reviews.create(newReview));
+      reject('Error with seeding the Reviews DB')
+    }
+  })
+}
+
+let refreshReviewDB = () => {
+  return new Promise((resolve, reject) => {
+    resolve(Reviews.deleteMany({}))
+  })
+}
+
+async function runReviewSeed() {
+  try {
+    await refreshReviewDB();
+    await seedReviewsDB();
+    console.log('Review DB seeded sucessfully.');
+  } catch (err) {
+    console.log('Error with seeding DB.')
   }
 }
 
-seedDB();
-// db.reviews.deleteMany({});
+runReviewSeed()
+// db.reviews.deleteMany({})
 
 
