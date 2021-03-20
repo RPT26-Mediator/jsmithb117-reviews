495fecc0cb5845015a23fd2bfacc2a2a646f6664
Nelson Ly <nelsonly123@gmail.com>
2021-03-20 10:30:06 -0700

    Refactored profile pic url in db to be an amazon S3 url.

--- database/db.js
+++ database/db.js
@@ -8,16 +8,6 @@ db.once('open', function() {
   console.log('connected to db')
 });
 
-let randomProfilePic = () => {
-  $.ajax({
-    url: 'https://randomuser.me/api/',
-    dataType: 'json',
-    success: (data) => {
-      return data.results[0].gender
-    }
-  });
-}
-
 let reviewSchema = new mongoose.Schema({
   userName: String,
   dateJoined: String,
@@ -42,7 +32,7 @@ let seedReviewsDB = () => {
       let newReview = new Reviews({
         userName: faker.name.firstName(),
         dateJoined: faker.date.month() + ' ' + faker.random.number({min:2010, max:2021}),
-        profilePic: 'https://randomuser.me/api/portraits/thumb/men/' + faker.random.number({min:0, max:99}) + '.jpg',
+        profilePic: `https://airbnbpp.s3-us-west-1.amazonaws.com/${faker.random.number({min:0, max:199})}.jpg`,
         reviewDescription: faker.lorem.sentences(3),
         reviewRating: {
           cleanliness: faker.random.number({min:1, max:5}),
