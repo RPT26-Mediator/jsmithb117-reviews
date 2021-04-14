f25659f0e10ff794b514b6af0b9d97ecf968782a
Nelson Ly <nelsonly123@gmail.com>
2021-04-14 00:16:48 -0700

    Added bottom margin for modal search + review rating

--- client/src/components/Modal/ReviewModal.jsx
+++ client/src/components/Modal/ReviewModal.jsx
@@ -26,7 +26,8 @@ const ReviewModal = (props) => (
               height:"24px",
               marginLeft: "25px",
               marginRight: "8px",
-              marginTop: "5px"
+              marginTop: "5px",
+              marginBottom: "10px"
               }}>
               <path strokeLinecap="butt" strokeLinejoin="butt" strokeWidth={0} d="M15.094 1.579l-4.124 8.885-9.86 1.27a1 1 0 0 0-.542 1.736l7.293 6.565-1.965 9.852a1 1 0 0 0 1.483 1.061L16 25.951l8.625 4.997a1 1 0 0 0 1.482-1.06l-1.965-9.853 7.293-6.565a1 1 0 0 0-.541-1.735l-9.86-1.271-4.127-8.885a1 1 0 0 0-1.814 0z" />
             </svg>
@@ -43,6 +44,7 @@ const ReviewModal = (props) => (
                   width: "550px",
                   height: "40px",
                   marginLeft: "30%",
+                  marginBottom: "10px",
                   paddingLeft: "15px",
                   borderRadius: "25px",
                   border: '1px solid black',
