b23072578ac37abca267333dbaccef0757c0e789
jsmithb117 <jsmithb117@gmail.com>
2021-04-28 20:38:21 -0700

    Adds put request to server.

--- server/index.js
+++ server/index.js
@@ -11,53 +11,74 @@ app.use(cors());
 app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-app.use('/:listingID', express.static(__dirname + '/../client/dist'));
+app.get('/:listingID', express.static(__dirname + '/../client/dist'));
 
 // this returns all the reviews
 app.get('/reviews', (req, res) => {
-  db.getAllReviews().then((reviews) => {
-    res.send(reviews)
-  }).catch((error) =>{
-    console.log(error)
-    res.end();
-  })
+  db.getAllReviews()
+    .then((reviews) => {
+      res.send(reviews)
+    })
+    .catch((error) =>{
+      console.log(error)
+      res.end();
+    });
 });
 
 // this returns all of the reviews for a specific listing
 app.get('/:listingID/reviews', (req, res) => {
-  db.getListingReviews(req.params.listingID).then((reviews) => {
-    res.send(reviews);
-  }).catch((error) =>{
-    console.log(error)
-    res.end();
-  })
+  db.getListingReviews(req.params.listingID)
+    .then((reviews) => {
+      res.send(reviews);
+    })
+    .catch((error) =>{
+      console.log(error)
+      res.end();
+    });
 });
 
 app.get('/:listingID/totalReviewCount', (req, res) => {
-  db.getListingTotalReviewCount(req.params.listingID).then((reviews) => {
-    if(reviews < 1) {
-      res.send('No reviews')
-    } else if (reviews === 1) {
-      res.send(reviews + ' review');
-    } else {
-      res.send(reviews + ' reviews');
-    }
-  }).catch((error) =>{
-    console.log(error)
-    res.end();
-  })
+  db.getListingTotalReviewCount(req.params.listingID)
+    .then((reviews) => {
+      if(reviews < 1) {
+        res.send('No reviews')
+      } else if (reviews === 1) {
+        res.send(reviews + ' review');
+      } else {
+        res.send(reviews + ' reviews');
+      }
+    })
+    .catch((error) =>{
+      console.log(error)
+      res.end();
+    });
 });
 
 // calc-ed in accordance to this quora response: https://www.quora.com/How-does-the-Airbnb-Rating-system-work
 app.get('/:listingID/averageReviewsRating', (req, res) => {
-  db.getAverageReviewRating(req.params.listingID).then((ratings) => {
-    let ratingsValue = Object.values(ratings[0]);
-    let averageRating = (ratingsValue.reduce((a,b) => a + b, 0 ))/(ratingsValue.length);
-    res.send({'averageRating': Math.round(averageRating * 100) / 100, 'ratings': ratings});
-  }).catch((error) => {
-    console.log(error)
-    res.end();
+  db.getAverageReviewRating(req.params.listingID)
+    .then((ratings) => {
+      let ratingsValue = Object.values(ratings[0]);
+      let averageRating = (ratingsValue.reduce((a,b) => a + b, 0 ))/(ratingsValue.length);
+      res.send({'averageRating': Math.round(averageRating * 100) / 100, 'ratings': ratings});
+    })
+    .catch((error) => {
+      console.log(error)
+      res.end();
+    });
+});
+
+app.put('/reviews', (req, res) => {
+  db.updateReview(req.body)
+  .then((document) => {
+    console.log('document: ', document);
+    res.status(201).send();
   })
+  .catch((err) => {
+    if (err) {
+      console.error('Error in PUT/reviews, err: ', err);
+    }
+  });
 });
 
 app.listen(PORT, () => {
