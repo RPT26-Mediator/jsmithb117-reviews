5973f36d75e1d6ba39e28a9c8481aee426c937da
jsmithb117 <jsmithb117@gmail.com>
2021-04-30 15:54:37 -0700

    Styling changes to db, setup eslint, gitignored .json files

--- database/db.js
+++ database/db.js
@@ -1,6 +1,8 @@
+/* eslint-disable no-console */
 const faker = require('faker');
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/airbnbReviewsDB', { useNewUrlParser: true , useUnifiedTopology: true});
+
+mongoose.connect('mongodb://localhost:27017/airbnbReviewsDB', { useNewUrlParser: true, useUnifiedTopology: true });
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
@@ -17,100 +19,92 @@ const reviewSchema = new mongoose.Schema({
   profilePic: String,
   reviewDescription: String,
   reviewRating: {
-      cleanliness: Number,
-      communication: Number,
-      checkIn: Number,
-      accuracy: Number,
-      location: Number,
-      value: Number
-    },
-  listingID: String
+    cleanliness: Number,
+    communication: Number,
+    checkIn: Number,
+    accuracy: Number,
+    location: Number,
+    value: Number,
+  },
+  listingID: String,
 }, { retainKeyOrder: true });
 
 const Reviews = mongoose.model('Reviews', reviewSchema);
 
-const seedReviewsDB = () => {
-  return new Promise((resolve, reject) => {
-    for (let i = 0; i < 1000; i++){
-      const newReview = new Reviews({
-        userName: faker.name.firstName(),
-        dateJoined: faker.date.month() + ' ' + faker.random.number({min:2010, max:2021}),
-        profilePic: `https://airbnbpp.s3-us-west-1.amazonaws.com/${faker.random.number({min:0, max:199})}.jpg`,
-        reviewDescription: faker.lorem.sentences(3),
-        reviewRating: {
-          cleanliness: faker.random.number({min:4, max:5}),
-          communication: faker.random.number({min:4, max:5}),
-          checkIn: faker.random.number({min:4, max:5}),
-          accuracy: faker.random.number({min:4, max:5}),
-          location: faker.random.number({min:4, max:5}),
-          value: faker.random.number({min:4, max:5}),
+const seedReviewsDB = () => new Promise((resolve, reject) => {
+  for (let i = 0; i < 1000; i++) {
+    const newReview = new Reviews({
+      userName: faker.name.firstName(),
+      dateJoined: `${faker.date.month()} ${faker.random.number({ min: 2010, max: 2021 })}`,
+      profilePic: `https://airbnbpp.s3-us-west-1.amazonaws.com/${faker.random.number({ min: 0, max: 199 })}.jpg`,
+      reviewDescription: faker.lorem.sentences(3),
+      reviewRating: {
+        cleanliness: faker.random.number({ min: 4, max: 5 }),
+        communication: faker.random.number({ min: 4, max: 5 }),
+        checkIn: faker.random.number({ min: 4, max: 5 }),
+        accuracy: faker.random.number({ min: 4, max: 5 }),
+        location: faker.random.number({ min: 4, max: 5 }),
+        value: faker.random.number({ min: 4, max: 5 }),
+      },
+      listingID: faker.random.number(100),
+    });
+    resolve(Reviews.create(newReview));
+    reject('Error with seeding the Reviews DB');
+  }
+});
+
+const refreshReviewDB = () => new Promise((resolve, reject) => {
+  resolve(Reviews.deconsteMany({}));
+  reject('Error with removing old data.');
+});
+
+const getAllReviews = () => new Promise((resolve, reject) => {
+  resolve(Reviews.find({}).sort({ listingID: 1 }));
+  reject('Error with getting all reviews.');
+});
+
+const getListingReviews = (listingID) => new Promise((resolve, reject) => {
+  resolve(Reviews.find({ listingID }));
+  reject('Error with getting this listings reviews.');
+});
+
+const getListingTotalReviewCount = (listingID) => new Promise((resolve, reject) => {
+  resolve(Reviews.find({ listingID }).countDocuments());
+  reject('Error with getting total review count.');
+});
+
+const getAverageReviewRating = (listingID) => new Promise((resolve, reject) => {
+  resolve(
+    Reviews.aggregate(
+      [
+        { $match: { listingID } },
+        {
+          $group: {
+            _id: '$listingID',
+            avg_clean: { $avg: '$reviewRating.cleanliness' },
+            avg_communication: { $avg: '$reviewRating.communication' },
+            avg_checkIn: { $avg: '$reviewRating.checkIn' },
+            avg_accuracy: { $avg: '$reviewRating.accuracy' },
+            avg_location: { $avg: '$reviewRating.location' },
+            avg_value: { $avg: '$reviewRating.value' },
+          },
         },
-        listingID: faker.random.number(100)
-      });
-      resolve(Reviews.create(newReview));
-      reject('Error with seeding the Reviews DB')
-    }
-  })
-}
-
-const refreshReviewDB = () => {
-  return new Promise((resolve, reject) => {
-    resolve(Reviews.deconsteMany({}));
-    reject('Error with removing old data.')
-  })
-}
-
-const getAllReviews = () => {
-  return new Promise((resolve, reject) => {
-    resolve(Reviews.find({}).sort({ "listingID": 1 }));
-    reject('Error with getting all reviews.')
-  })
-}
-
-const getListingReviews = (listingID) => {
-  return new Promise((resolve, reject) => {
-    resolve(Reviews.find({ listingID }));
-    reject('Error with getting this listings reviews.')
-  })
-}
-
-const getListingTotalReviewCount = (listingID) => {
-  return new Promise((resolve, reject) => {
-    resolve(Reviews.find({'listingID' : listingID }).countDocuments());
-    reject('Error with getting total review count.')
-  })
-}
-
-const getAverageReviewRating = (listingID) => {
-  return new Promise((resolve, reject) => {
-    resolve(
-      Reviews.aggregate(
-        [
-          {$match: {listingID}},
-          {$group: {
-            "_id":"$listingID",
-            avg_clean:{"$avg" : "$reviewRating.cleanliness"},
-            avg_communication:{"$avg" : "$reviewRating.communication"},
-            avg_checkIn:{"$avg" : "$reviewRating.checkIn"},
-            avg_accuracy:{"$avg" : "$reviewRating.accuracy"},
-            avg_location:{"$avg" : "$reviewRating.location"},
-            avg_value:{"$avg" : "$reviewRating.value"}
-          }},
-          {$project:{
+        {
+          $project: {
             _id: 0,
-            rounded_clean:{"$trunc" : ["$avg_clean", 1]},
-            rounded_communication:{"$trunc" : ["$avg_communication", 1]},
-            rounded_checkIn:{"$trunc" : ["$avg_checkIn",1]},
-            rounded_accuracy:{"$trunc" : ["$avg_accuracy",1]},
-            rounded_location:{"$trunc" : ["$avg_location",1]},
-            rounded_value:{"$trunc" : ["$avg_value",1]}
-          }}
-        ]
-    )
+            rounded_clean: { $trunc: ['$avg_clean', 1] },
+            rounded_communication: { $trunc: ['$avg_communication', 1] },
+            rounded_checkIn: { $trunc: ['$avg_checkIn', 1] },
+            rounded_accuracy: { $trunc: ['$avg_accuracy', 1] },
+            rounded_location: { $trunc: ['$avg_location', 1] },
+            rounded_value: { $trunc: ['$avg_value', 1] },
+          },
+        },
+      ],
+    ),
   );
-  reject('Error with getting average rating.')
-  });
-}
+  reject('Error with getting average rating.');
+});
 
 async function runReviewSeed() {
   try {
@@ -118,37 +112,29 @@ async function runReviewSeed() {
     await seedReviewsDB();
     console.log('Review DB seeded sucessfully.');
   } catch (err) {
-    console.log('Error with seeding DB.')
+    console.log('Error with seeding DB.');
   }
 }
 
-const insertReview = (review) => {
-  return new Promise((resolve, reject) => {
-    resolve(Reviews.create(review));
-    reject('Error inserting a review.');
-  })
-};
+const insertReview = (review) => new Promise((resolve, reject) => {
+  resolve(Reviews.create(review));
+  reject('Error inserting a review.');
+});
 
-const updateReview = (review) => {
-  return new Promise((resolve, reject) => {
-    resolve(Reviews.replaceOne({ _id: review._id }, review));
-    reject('Error updating a review');
-  });
-};
+const updateReview = (review) => new Promise((resolve, reject) => {
+  resolve(Reviews.replaceOne({ _id: review._id }, review));
+  reject('Error updating a review');
+});
 
-const deleteReview = (_id) => {
-  return new Promise((resolve, reject) => {
-    resolve(Reviews.deleteOne({ _id }));
-    reject('Error with getting total review count.')
-  });
-};
+const deleteReview = (_id) => new Promise((resolve, reject) => {
+  resolve(Reviews.deleteOne({ _id }));
+  reject('Error with getting total review count.');
+});
 
-const getOneReview = (_id) => {
-  return new Promise((resolve, reject) => {
-    resolve(Reviews.find({ _id }));
-    reject('Error with getting this listings reviews.')
-  })
-};
+const getOneReview = (_id) => new Promise((resolve, reject) => {
+  resolve(Reviews.find({ _id }));
+  reject('Error with getting this listings reviews.');
+});
 
 module.exports = {
   getAllReviews,
@@ -159,5 +145,5 @@ module.exports = {
   insertReview,
   updateReview,
   deleteReview,
-  getOneReview
+  getOneReview,
 };
