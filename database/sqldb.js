5b39a783c5ff9a5e58b6063d17daa340f02967c0
jsmithb117 <jsmithb117@gmail.com>
2021-05-18 11:21:53 -0700

    Postgres database methods

--- database/sqldb.js
+++ database/sqldb.js
@@ -1,18 +1,20 @@
 /* eslint-disable arrow-body-style */
 const { Sequelize } = require('sequelize');
 
-const sqldb = new Sequelize('postgres://postgres:password@localhost:5432/sdc', {
+const options = {
+  logging: false,
   define: {
     timestamps: false,
   },
-  underscored: true,
   dialect: 'postgres',
-});
+};
 
-const Review = sqldb.define('reviews', {
+const postgres = new Sequelize('postgres://postgres:password@localhost:5432/sdc', options);
+
+const Review = postgres.define('reviews', {
   id: {
     type: Sequelize.INTEGER,
-    autoIncrement: false,
+    autoIncrement: true,
     primaryKey: true,
   },
   user_name: Sequelize.STRING,
@@ -21,70 +23,97 @@ const Review = sqldb.define('reviews', {
   review_description: Sequelize.STRING,
   cleanliness: Sequelize.INTEGER,
   communication: Sequelize.INTEGER,
-  check_in: Sequelize.INTEGER,
+  checkin: Sequelize.INTEGER,
   accuracy: Sequelize.INTEGER,
   location: Sequelize.INTEGER,
   value: Sequelize.INTEGER,
   listing_id: Sequelize.INTEGER,
 });
 
-const ListingID = sqldb.define('listingids', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: false,
-    primaryKey: true,
-  },
-});
-
-ListingID.hasMany(Review, { foreignKey: 'id' });
-Review.belongsTo(ListingID, { foreignKey: 'listing_id' });
-
-const insertOneReview = (review) => {
+postgres.insertOneReview = (review) => {
   const reviewObject = {
-    id: review.id,
     user_name: review.userName,
     date_joined: review.dateJoined,
     profile_pic: review.profilePic,
     review_description: review.reviewDescription,
     cleanliness: review.reviewRating.cleanliness,
     communication: review.reviewRating.communication,
-    check_in: review.reviewRating.checkIn,
+    checkin: review.reviewRating.checkIn,
     accuracy: review.reviewRating.accuracy,
     location: review.reviewRating.location,
     value: review.reviewRating.value,
     listing_id: review.listingID,
   };
-  Review.create(reviewObject);
+  return new Promise((resolve, reject) => {
+    resolve(Review.create(reviewObject));
+    reject(new Error('Error in insertOneReview'));
+  })
 };
 
-const insertOneListing = (id) => {
-  ListingID.sync();
+postgres.insertOneListing = (id) => {
   return new Promise((resolve, reject) => {
     resolve(ListingID.findOrCreate({ where: { id } }));
     reject(new Error('Error in insertOneListing'));
   });
 };
 
-const deleteOneListing = (id) => {
+postgres.bulkCreateReviews = (array) => {
   return new Promise((resolve, reject) => {
-    resolve(ListingID.destroy({ where: { id } }));
-    reject(new Error('Error in deleteOneListing'));
+    resolve(Review.bulkCreate(array));
+    reject(new Error('Error in postgres.bulkCreateReviews'));
   });
-};
+}
 
-const deleteOneReview = (id) => {
+postgres.deleteOneReview = (id) => {
   return new Promise((resolve, reject) => {
     resolve(Review.destroy({ where: { id } }));
     reject(new Error('Error in deleteOneReview'));
   });
 };
 
-module.exports = {
-  deleteOneReview,
-  deleteOneListing,
-  insertOneListing,
-  insertOneReview,
-  sqldb,
-  Review,
-  ListingID,
+postgres.readAllByID = (listing_id) => {
+  return new Promise((resolve, reject) => {
+    resolve(Review.findAll({ where: { listing_id } }));
+    reject(new Error('Error in postgresReadOneByID'));
+  })
 };
+
+postgres.getAverageReviewRating = async (listing_id) => {
+  const listings = await Review.findAll({ where: { listing_id } });
+  let sums = 0;
+  const count = listings.length;
+  for (let i = 0; i < listings.length; i++) {
+    sums += listings[i].dataValues.cleanliness;
+    sums += listings[i].dataValues.communication;
+    sums += listings[i].dataValues.checkin;
+    sums += listings[i].dataValues.accuracy;
+    sums += listings[i].dataValues.location;
+    sums += listings[i].dataValues.value;
+  }
+  const averageReviewRating = sums / 6 / count;
+  return new Promise((resolve, reject) => {
+    resolve(averageReviewRating);
+    reject(new Error('Error in postgres.getAverageReviewRating'));
+  });
+};
+
+postgres.update = (body) => {
+  return new Promise((resolve, reject) => {
+    const query = {
+      user_name: body.userName,
+      date_joined: body.dateJoined,
+      profile_pic: body.profilePic,
+      review_description: body.reviewDescription,
+        cleanliness: body.reviewRating.cleanliness,
+        communication: body.reviewRating.communication,
+        checkin: body.reviewRating.checkIn,
+        accuracy: body.reviewRating.accuracy,
+        location: body.reviewRating.location,
+        value: body.reviewRating.value
+    };
+    resolve(Review.update(query, {where: {id: body.id}}));
+    reject(new Error('Error in postgres.update'));
+  });
+};
+
+module.exports = postgres;
