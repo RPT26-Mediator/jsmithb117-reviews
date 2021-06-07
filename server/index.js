186a83860932996a997907b20bbf61fc904ee535
jsmithb117 <jsmithb117@gmail.com>
2021-06-07 10:11:44 -0700

    Added newRelic error handling

--- server/index.js
+++ server/index.js
@@ -4,7 +4,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const compression = require('compression');
 const cors = require('cors');
-const postgres =  require('../database/sqldb.js');
+const postgres = require('../database/sqldb.js');
+const newRelic = require('newrelic');
 
 const PORT = 3006;
 const app = express();
@@ -16,32 +17,31 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/:listingID', express.static(`${__dirname}/../client/dist`));
 
 const handleError = (err, res, location, status) => {
-  if (err) {
-    console.error(`Error in ${location}: `, err);
-    res.sendStatus(status);
-  }
+  newRelic.noticeError(err);
+  console.error(`Error in ${location}: `, err);
+  res.sendStatus(status);
 };
 
 // Create
 app.post('/insertreview', (req, res) => {
   postgres.insertOneReview(req.body)
-  .then((dbResponse) => {
-    res.send(dbResponse);
-  })
-  .catch((err) => {
-    handleError(err, res, 'app.post/insertreview', 500);
-  });
+    .then((dbResponse) => {
+      res.send(dbResponse);
+    })
+    .catch((err) => {
+      handleError(err, res, 'app.post/insertreview', 500);
+    });
 });
 
 // Read all reviews for a given listingID
 app.get('/:listingID/reviews', (req, res) => {
   postgres.readAllByID(req.params.listingID)
-  .then((dbResponse) => {
-    res.send(dbResponse);
-  })
-  .catch((err) => {
-    handleError(err, res, 'app.get/reviewpostgres', 500);
-  });
+    .then((dbResponse) => {
+      res.send(dbResponse);
+    })
+    .catch((err) => {
+      handleError(err, res, 'app.get/reviewpostgres', 500);
+    });
 });
 
 // Read number of reviews for a given listingID
@@ -49,8 +49,8 @@ app.get('/:listingID/totalReviewCount', (req, res) => {
   postgres.readAllByID(req.params.listingID)
     .then((dbResponse) => {
       const reviewResponse = dbResponse.length === 0 ? 'No Reviews'
-      : dbResponse.length === 1 ? '1 review'
-      : `${dbResponse.length} reviews`;
+        : dbResponse.length === 1 ? '1 review'
+          : `${dbResponse.length} reviews`;
       res.send(reviewResponse);
     })
     .catch((err) => {
@@ -61,12 +61,12 @@ app.get('/:listingID/totalReviewCount', (req, res) => {
 // Read average rating for a given listingID
 app.get('/:listingID/averageReviewsRating', (req, res) => {
   postgres.getAverageReviewRating(req.params.listingID)
-  .then((functionResponse) => {
-    res.send(functionResponse);
-  })
-  .catch((err) => {
-    handleError(err, res, 'app.get/:listingID/averageReviewsRating', 500);
-  });
+    .then((functionResponse) => {
+      res.send(functionResponse);
+    })
+    .catch((err) => {
+      handleError(err, res, 'app.get/:listingID/averageReviewsRating', 500);
+    });
 });
 
 // Update
@@ -84,12 +84,12 @@ app.put('/reviewpostgres', (req, res) => {
 app.delete('/review', (req, res) => {
   if (req.body.id) {
     postgres.deleteOneReview(req.body.id)
-    .then((dbResponse) => {
-      res.send(JSON.stringify(dbResponse));
-    })
-    .catch((err) => {
-      handleError(err, res, 'app.delete/review', 500);
-    });
+      .then((dbResponse) => {
+        res.send(JSON.stringify(dbResponse));
+      })
+      .catch((err) => {
+        handleError(err, res, 'app.delete/review', 500);
+      });
   } else {
     handleError(err, res, 'app.delete/review', 404);
   }
