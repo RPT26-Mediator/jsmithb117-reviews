b07736d670d37a18846564cc10d1eaa72e1763d4
jsmithb117 <jsmithb117@gmail.com>
2021-06-10 13:18:19 -0700

    Commented out unnecessary methods

--- database/sqldb.js
+++ database/sqldb.js
@@ -1,76 +1,81 @@
 /* eslint-disable arrow-body-style */
-const { Sequelize } = require('sequelize');
+// const { Sequelize } = require('sequelize');
 
-const options = {
-  logging: false,
-  define: {
-    timestamps: false,
-  },
-  dialect: 'postgres',
-};
+// const options = {
+//   logging: false,
+//   define: {
+//     timestamps: false,
+//   },
+//   dialect: 'postgres',
+// };
 
-const postgres = new Sequelize('postgres://postgres:password@localhost:5432/reviews', options);
+// const postgres = new Sequelize('postgres://postgres:password@localhost:5432/reviews', options);
+const postgres = {};
 
-const Review = postgres.define('reviews', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    primaryKey: true,
-  },
-  "userName": Sequelize.STRING,
-  "dateJoined": Sequelize.STRING,
-  "profilePic": Sequelize.STRING,
-  "reviewDescription": Sequelize.STRING,
-  "reviewRating": Sequelize.JSON,
-  "listingID": Sequelize.INTEGER,
-});
+// const Review = postgres.define('reviews', {
+//   id: {
+//     type: Sequelize.INTEGER,
+//     autoIncrement: true,
+//     primaryKey: true,
+//   },
+//   "userName": Sequelize.STRING,
+//   "dateJoined": Sequelize.STRING,
+//   "profilePic": Sequelize.STRING,
+//   "reviewDescription": Sequelize.STRING,
+//   "reviewRating": Sequelize.JSON,
+//   "listingID": Sequelize.INTEGER,
+// });
 
-postgres.insertOneReview = (review) => {
-  return new Promise((resolve, reject) => {
-    resolve(Review.create(review));
-    reject(new Error('Error in insertOneReview'));
-  })
-};
+// postgres.insertOneReview = (review) => {
+//   return new Promise((resolve, reject) => {
+//     resolve(Review.create(review));
+//     reject(new Error('Error in insertOneReview'));
+//   })
+// };
 
-postgres.insertOneListing = (id) => {
-  return new Promise((resolve, reject) => {
-    resolve(ListingID.findOrCreate({ where: { id } }));
-    reject(new Error('Error in insertOneListing'));
-  });
-};
+// postgres.insertOneListing = (id) => {
+//   return new Promise((resolve, reject) => {
+//     resolve(ListingID.findOrCreate({ where: { id } }));
+//     reject(new Error('Error in insertOneListing'));
+//   });
+// };
 
-postgres.bulkCreateReviews = (array) => {
-  return new Promise((resolve, reject) => {
-    resolve(Review.bulkCreate(array));
-    reject(new Error('Error in postgres.bulkCreateReviews'));
-  });
-}
+// postgres.bulkCreateReviews = (array) => {
+//   return new Promise((resolve, reject) => {
+//     resolve(Review.bulkCreate(array));
+//     reject(new Error('Error in postgres.bulkCreateReviews'));
+//   });
+// }
 
-postgres.deleteOneReview = (id) => {
-  return new Promise((resolve, reject) => {
-    resolve(Review.destroy({ where: { id } }));
-    reject(new Error('Error in deleteOneReview'));
-  });
-};
+// postgres.deleteOneReview = (id) => {
+//   return new Promise((resolve, reject) => {
+//     resolve(Review.destroy({ where: { id } }));
+//     reject(new Error('Error in deleteOneReview'));
+//   });
+// };
 
-postgres.readAllByID = (listingID) => {
-  return new Promise((resolve, reject) => {
-    const query = `SELECT * FROM reviews where "listingID" = ${listingID}`;
-    resolve(postgres.query(query)
-      .then((dbResponse) => {
-        return dbResponse[0];
-      })
-      .catch((err) => {
-        if (err) {
-          console.error('Error in postgres.readAllByID: ', err);
-        }
-      }));
-    reject(new Error('Error in postgresReadOneByID'));
-  })
-};
+// postgres.readAllByID = (listingID) => {
+//   return new Promise((resolve, reject) => {
+//     const query = `SELECT * FROM reviews where "listingID" = ${listingID}`;
+//     resolve(postgres.query(query)
+//       .then((dbResponse) => {
+//         return dbResponse[0];
+//       })
+//       .catch((err) => {
+//         if (err) {
+//           console.error('Error in postgres.readAllByID: ', err);
+//         }
+//       }));
+//     reject(new Error('Error in postgresReadOneByID'));
+//   })
+// };
 
 postgres.getAverageReviewRating = async (listingID) => {
-  const listings = await Review.findAll({ where: { listingID } });
+  const listings = await fetch(`http://54.215.82.50:80/${req.params.listingID}/reviews`)
+    .catch((err) => {
+      console.error('Error in postgres.getAverageReviewRating: ', err);
+    });
+  // const listings = await Review.findAll({ where: { listingID } });
   let sums = {
     cleanliness: 0,
     communication: 0,
@@ -121,23 +126,23 @@ postgres.getAverageReviewRating = async (listingID) => {
   });
 };
 
-postgres.update = (body) => {
-  return new Promise((resolve, reject) => {
-    const query = {
-      user_name: body.userName,
-      date_joined: body.dateJoined,
-      profile_pic: body.profilePic,
-      review_description: body.reviewDescription,
-      cleanliness: body.reviewRating.cleanliness,
-      communication: body.reviewRating.communication,
-      checkin: body.reviewRating.checkIn,
-      accuracy: body.reviewRating.accuracy,
-      location: body.reviewRating.location,
-      value: body.reviewRating.value
-    };
-    resolve(Review.update(query, { where: { id: body.id } }));
-    reject(new Error('Error in postgres.update'));
-  });
-};
+// postgres.update = (body) => {
+//   return new Promise((resolve, reject) => {
+//     const query = {
+//       user_name: body.userName,
+//       date_joined: body.dateJoined,
+//       profile_pic: body.profilePic,
+//       review_description: body.reviewDescription,
+//       cleanliness: body.reviewRating.cleanliness,
+//       communication: body.reviewRating.communication,
+//       checkin: body.reviewRating.checkIn,
+//       accuracy: body.reviewRating.accuracy,
+//       location: body.reviewRating.location,
+//       value: body.reviewRating.value
+//     };
+//     resolve(Review.update(query, { where: { id: body.id } }));
+//     reject(new Error('Error in postgres.update'));
+//   });
+// };
 
 module.exports = postgres;
